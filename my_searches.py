c3e2e583d1298a4b56fedc79dbeb8c58be594e8f
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2021-11-29 13:53:39 -0800

    Update my searches

--- my_searches.py
+++ my_searches.py
@@ -14,17 +14,20 @@ import arxiv_search_function as asf
 import biomedrxiv_search_function as bmsf
 import datetime
 
+# Notes: 
+# - You can include 'cs' in the subjects, but this will increase the number of results.
+# - Keywords are searched in the title *and abstract*
+
 records_df = asf.arxivsearch(
 	start_date = datetime.date.today() - datetime.timedelta(days=7),
  	end_date = datetime.date.today(), 
-	subjects = ['stat', 'math'], 
+	subjects = ['stat', 'math', 'cs'], 
 	kwd_req = [], 
-	kwd_exc = [], 
-	kwd_one = [['parallel'], ['Bayes'], ['sample'], ['sampler'], ['sampling'], ['Monte'], 
-    ['Markov'], ['tempering'], ['tempered'], ['variational'], ['scalable'], ['ergodic'], ['Gibbs'], ['Metropolis'], 
-    ['Hamiltonian'], ['MCMC']], 
-	cols = ['id', 'url', 'title', 'categories', 'abstract', 'authors', 'date'])
+	kwd_exc = ['autoencoder'], 
+	kwd_one = [['Monte Carlo', 'Markov chain', 'variational inference', 'ergodic', 
+	'Gibbs', 'Metropolis', 'Hamiltonian Monte Carlo', 'HMC', 'MCMC', 'SMC', 'PDMP', 'PDMC']], 
+	cols = ['id', 'url', 'title', 'categories', 'abstract', 'authors', 'date'],
+	export='search_results/',
+	max_records=1000)
 print(records_df.title)
-
-
-# subjects = ['cs', 'stat', 'math'],
+print(records_df)
